d9ccd182432670a9d958e8247ba973fda1cc53e1
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-12-03 15:24:25 +0300

    add bind rate game button

--- Assets/Script/UiMenu.cs
+++ Assets/Script/UiMenu.cs
@@ -13,21 +13,36 @@ public class UiMenu : MonoBehaviour
     [Header("Score")]
     [SerializeField] private TextMeshProUGUI _bestScore;
 
+    [Header("Rate Game")]
+    [SerializeField] private Button _rateGame;
+
     private void OnEnable()
     {
         _started.onClick.AddListener(_levelStarter.StartLevel);
+        _rateGame.onClick.AddListener(OnRateGame);
+
         YandexGame.GetDataEvent += OnLoadData;
     }
 
     private void OnDisable()
     {
         _started.onClick.RemoveListener(_levelStarter.StartLevel);
+        _rateGame.onClick.RemoveListener(OnRateGame);
+
         YandexGame.GetDataEvent -= OnLoadData;
     }
 
     private void OnLoadData()
     {
+        if (YandexGame.auth == false)
+            _rateGame.DisactivateSelf();
+
         UpdateBestScore(YandexGame.savesData.bestScore);
+    }
+
+    private void OnRateGame()
+    {
+        throw new NotImplementedException();
     }
 
     public void UpdateBestScore(long bestScore)
